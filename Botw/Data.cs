dbe9abefcb799836e3763e4cd49986e434a4abbe
ArchLeaders <archparliament@gmail.com>
2021-10-23 14:45:06 -0700

    Proper data holding and Json settings file.

---
+++ Botw/Data.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Botw
+{
+    public class Data
+    {
+        // The Data class stores all of the paths used by BMM
+
+        public static void WriteSettings(string cemu, string baseG, string updateG, string dlcG = null, string gameG_NX = null, string dlcG_NX = null)
+        {
+            Json.Settings settings = new();
+            JsonSerializer serializer = new();
+
+            settings.cemu_dir = cemu;
+            settings.game_dir = baseG;
+            settings.update_dir = updateG;
+            settings.dlc_dir = dlcG;
+            settings.game_dir_nx = gameG_NX;
+            settings.dlc_dir_nx = dlcG_NX;
+
+            using (StreamWriter streamWriter = new StreamWriter($"{root}\\settings.json"))
+                using (JsonWriter jsonWriter = new JsonTextWriter(streamWriter))
+                    serializer.Serialize(jsonWriter, settings);
+        }
+
+        public static string root = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Botw-MM";
+    }
+}
---
+++ Botw/Json/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botw.Json
+{
+    public class Settings
+    {
+        public string cemu_dir { get; set; }
+        public string game_dir { get; set; }
+        public string game_dir_nx { get; set; }
+        public string update_dir { get; set; }
+        public string dlc_dir { get; set; }
+        public string dlc_dir_nx { get; set; }
+    }
+}
