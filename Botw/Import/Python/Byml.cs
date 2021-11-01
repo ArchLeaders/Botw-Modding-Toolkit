43604b7ee5bb1b22ca2da3083a553eb85fe41800
ArchLeaders <archparliament@gmail.com>
2021-11-01 13:32:08 -0700

    Added BYMl Processes.

--- Botw/Import/Python/Byml.cs
+++ Botw/Import/Python/Byml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Botw.System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +9,29 @@ namespace Botw.Import.Python
 {
     public class Byml
     {
-        public static async Task BymlToYml(string input, string output = null, string endian = "-b", string format = null)
+        public static async Task BymlToYml(string input, string output = null, string endian = "-b", bool yaz0 = false)
         {
-            throw new NotImplementedException();
+            string _yaz0 = "";
+            if (yaz0) _yaz0 = "s";
+
+            if (output == null)
+                output = $"!!.{_yaz0}{input.Split('.')[input.Split('.').Length - 1]}";
+            else
+            {
+                string extension = $"{ _yaz0}{input.Split('.')[input.Split('.').Length - 1]}";
+                string file = output.Replace($".{input.Split('.')[input.Split('.').Length - 1]}", "");
+                output = $"{file}.{extension}";
+            }
+
+
+            await Proc.Async("byml_to_yml.exe", $"\"{input}\" \"{output}\" {endian}");
         }
 
-        public static async Task YmlToByml()
+        public static async Task YmlToByml(string input, string output = null)
         {
-            throw new NotImplementedException();
+            if (output == null) output = $"!!.yml";
+
+            await Proc.Async("byml_to_yml.exe", $"\"{input}\" \"{output}\"");
         }
     }
 }
--- Botw/System/Proc.cs
+++ Botw/System/Proc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,32 @@ namespace Botw.System
 {
     public class Proc
     {
-        public static async Task Silent()
+        public static async Task Async(string exe, string args = null, bool wait = true, bool silent = true, ProcessWindowStyle style = ProcessWindowStyle.Normal)
         {
-            throw new NotImplementedException();
+            Process process = new();
+
+            process.StartInfo.FileName = exe;
+            process.StartInfo.Arguments = args;
+            process.StartInfo.CreateNoWindow = silent;
+            process.StartInfo.WindowStyle = style;
+            if (silent) process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            await Task.Run(() => process.Start());
+            if (wait) await process.WaitForExitAsync();
+        }
+
+        public static void Syncronous(string exe, string args = null, bool wait = true, bool silent = true, ProcessWindowStyle style = ProcessWindowStyle.Normal)
+        {
+            Process process = new();
+
+            process.StartInfo.FileName = exe;
+            process.StartInfo.Arguments = args;
+            process.StartInfo.CreateNoWindow = silent;
+            process.StartInfo.WindowStyle = style;
+            if (silent) process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            process.Start();
+            if (wait) process.WaitForExit();
         }
     }
 }
